30be9d4e269235652eb1b687a9d5ad742274f277
Peter Zorzonello <zorzonp@bu.edu>
2018-10-07 10:36:00 -0400

    Update the test

--- LAMP Server Files/demo.php
+++ LAMP Server Files/demo.php
@@ -1,10 +1,18 @@
 <html>
+<!--*****************************************************************************************
+*                                                                                           *
+* Author:      Peter Zorzonello                                                             *
+* Class:       EC601-A1                                                                     *
+* File:        demo.php                                                                     *
+* Description: This file is a demo used to test the PHP functions to add users, add owners, * 
+*              and retrieve info.                                                           *
+*********************************************************************************************-->
         <head>
         </head>
         <body>
                 <div>
                         <h1 style="color:red">DO NOT PUT REAL PERSONAL INFO IN THIS FORM!!!!</h1>
-
+                        <h3>Test Add User</h3>
                         <form action="add_customer.php" method="post" id="form">
                                 <input type="text" name="first_name" id="first_name" placeholder="First Name"></input><br/>
                                 <input type="text" name="last_name" id="last_name" placeholder="Last Name"></input><br/>
@@ -20,18 +28,34 @@
 
                                 <input type="text" name="username" placeholder="Username"></input><br/>
                                 <input type="text" name="password" placeholder="Password"></input><br/>
-                                <input type="text" name="venmo_username" placeholder="Venmo Username"></input><br/>
+                                <input type="text" name="venmo_username" placeholder="Venmo Username"></input><br/> 
                                 <input type="text" name="salt" placeholder="1234567890"></input><br/>
                                 <input type="submit" name="submit" id="submit" value="Submit">
                         </form>
-                        
+                        <h3>Test lookup</h3>    
                         <form action="lookup_license.php" method="post" id="form">
                                 <input type="text" name="license_plate" placeholder="License Plate Number"></input><br/>
                                 <input type="text" name="state" placeholder="State"></input><br/>
+                                <input type="text" name="username" placeholder="Username"></input><br/>
+                                <input type="text" name="password" placeholder="Password"></input><br/>
                                 <input type="submit" name="submit" id="submit" value="Submit">
                         </form>
                 </div>
+                
                 <div>
+                        <h3>Test Add Owner</h3>
+                        <form action="add_owner.php" method="post" id="form">
+                                <input type="text" name="username" placeholder="Username"></input><br/>
+                                <input type="text" name="password" placeholder="Password"></input><br/>
+                                <input type="text" name="salt" placeholder="1234567890"></input><br/>
+                                <input type="submit" name="submit" id="submit" value="Submit">
+                        </form>
+                                        
+                        <h3>Test Get Salt</h3>
+                        <form action="get_salt.php" method="post" id="form">
+                                <input type="text" name="username" placeholder="Username"></input><br/>
+                                <input type="submit" name="submit" id="submit" value="Submit">
+                        </form>
                 </div>
         </body>
 </html>
