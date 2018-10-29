34134218036f0b88b28dceca5c6600cff7df3893
Peter Zorzonello <zorzonp@bu.edu>
2018-10-29 16:15:01 -0400

    Add a new test

--- LAMP Server Files/demo.php
+++ LAMP Server Files/demo.php
@@ -206,6 +206,12 @@
                                 <input type="text" name="type" placeholder="Type"></input><br/>
                                 <input type="submit" name="submit" id="submit" value="Submit">
                         </form>
+                        <h3>Test Get Self</h3>
+                        <form action="get_self.php" method="post" id="form">
+                                <input type="text" name="username" placeholder="Username"></input><br/>
+                                <input type="text" name="password" placeholder="Password"></input><br/>
+                                <input type="submit" name="submit" id="submit" value="Submit">
+                        </form>
                 </div>
         </body>
 </html>
