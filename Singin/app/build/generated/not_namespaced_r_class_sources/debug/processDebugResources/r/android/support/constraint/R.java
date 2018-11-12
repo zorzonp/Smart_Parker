7a5d171bd982f55394180b9dd99d5ffb2c2d3846
xcliang <xcliang@bu.edu>
2018-11-12 17:53:57 -0500

    temp

--- Singin/app/build/generated/not_namespaced_r_class_sources/debug/processDebugResources/r/android/support/constraint/R.java
+++ Singin/app/build/generated/not_namespaced_r_class_sources/debug/processDebugResources/r/android/support/constraint/R.java
@@ -62,13 +62,13 @@ public final class R {
 
         public static final int all = 0x7f07001c;
         public static final int basic = 0x7f07001f;
-        public static final int chains = 0x7f070028;
-        public static final int none = 0x7f070050;
-        public static final int packed = 0x7f070055;
-        public static final int parent = 0x7f070056;
-        public static final int spread = 0x7f070074;
-        public static final int spread_inside = 0x7f070075;
-        public static final int wrap = 0x7f07008f;
+        public static final int chains = 0x7f07002d;
+        public static final int none = 0x7f070054;
+        public static final int packed = 0x7f070059;
+        public static final int parent = 0x7f07005a;
+        public static final int spread = 0x7f070078;
+        public static final int spread_inside = 0x7f070079;
+        public static final int wrap = 0x7f070093;
     }
     public static final class styleable {
         private styleable() {}
--- Singin/app/src/main/java/com/example/xcliang/singin/Signin.java
+++ Singin/app/src/main/java/com/example/xcliang/singin/Signin.java
@@ -2,6 +2,12 @@ package com.example.xcliang.singin;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.content.Intent;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.app.Activity;
+import android.view.View;
+
 
 public class Signin extends AppCompatActivity {
 
@@ -9,5 +15,58 @@ public class Signin extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signin);
+
+        Button btn1=(Button)findViewById(R.id.btn_button1);
+        btn1.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                gotoLayout2();
+            }
+        });
+
     }
+
+    private void gotoLayout2(){
+        setContentView(R.layout.activity_signin2);
+        Button btn2=(Button)findViewById(R.id.btn_button2_1);
+        Button btn3=(Button)findViewById(R.id.btn_button2_2);
+
+        btn2.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                gotoLayout1();
+            }
+        });
+
+        btn3.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                gotoLayout3();
+            }
+        });
+    }
+
+    private void gotoLayout1(){
+        setContentView(R.layout.activity_signin);
+        Button btn1=(Button)findViewById(R.id.btn_button1);
+        btn1.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                gotoLayout2();
+            }
+        });
+    }
+
+    private void gotoLayout3(){
+        setContentView(R.layout.activity_signin3);
+        Button btn4=(Button)findViewById(R.id.btn_button3_1);
+        btn4.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                gotoLayout2();
+            }
+        });
+    }
+
+
 }
