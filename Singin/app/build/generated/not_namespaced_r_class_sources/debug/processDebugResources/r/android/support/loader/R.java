19c7c73f55b2c731e414b95ba4dfb2f88f5cf8f0
Peter Zorzonello <zorzonp@bu.edu>
2018-12-03 16:44:29 -0500

    Revert "Merge branch 'Java_Integ' of https://github.com/zorzonp/Smart_Parker into Java_Integ"
    This reverts commit 23896ccbc25f00d4d42d98596b600b959fbd4948, reversing
    changes made to 32c20c03a18eab8301b0888c9668b1fa73466253.

--- Singin/app/build/generated/not_namespaced_r_class_sources/debug/processDebugResources/r/android/support/loader/R.java
+++ Singin/app/build/generated/not_namespaced_r_class_sources/debug/processDebugResources/r/android/support/loader/R.java
@@ -85,27 +85,27 @@ public final class R {
         public static final int actions = 0x7f070017;
         public static final int async = 0x7f07001d;
         public static final int blocking = 0x7f070020;
-        public static final int chronometer = 0x7f070033;
-        public static final int forever = 0x7f07004a;
-        public static final int icon = 0x7f07004e;
-        public static final int icon_group = 0x7f07004f;
-        public static final int info = 0x7f070052;
-        public static final int italic = 0x7f070053;
-        public static final int line1 = 0x7f070058;
-        public static final int line3 = 0x7f070059;
-        public static final int normal = 0x7f070065;
-        public static final int notification_background = 0x7f070066;
-        public static final int notification_main_column = 0x7f070067;
-        public static final int notification_main_column_container = 0x7f070068;
-        public static final int right_icon = 0x7f070073;
-        public static final int right_side = 0x7f070074;
-        public static final int tag_transition_group = 0x7f070096;
-        public static final int tag_unhandled_key_event_manager = 0x7f070097;
-        public static final int tag_unhandled_key_listeners = 0x7f070098;
-        public static final int text = 0x7f070099;
-        public static final int text2 = 0x7f07009a;
-        public static final int time = 0x7f0700b8;
-        public static final int title = 0x7f0700b9;
+        public static final int chronometer = 0x7f070032;
+        public static final int forever = 0x7f070049;
+        public static final int icon = 0x7f07004d;
+        public static final int icon_group = 0x7f07004e;
+        public static final int info = 0x7f070051;
+        public static final int italic = 0x7f070052;
+        public static final int line1 = 0x7f070057;
+        public static final int line3 = 0x7f070058;
+        public static final int normal = 0x7f070064;
+        public static final int notification_background = 0x7f070065;
+        public static final int notification_main_column = 0x7f070066;
+        public static final int notification_main_column_container = 0x7f070067;
+        public static final int right_icon = 0x7f070072;
+        public static final int right_side = 0x7f070073;
+        public static final int tag_transition_group = 0x7f070094;
+        public static final int tag_unhandled_key_event_manager = 0x7f070095;
+        public static final int tag_unhandled_key_listeners = 0x7f070096;
+        public static final int text = 0x7f070097;
+        public static final int text2 = 0x7f070098;
+        public static final int time = 0x7f0700b6;
+        public static final int title = 0x7f0700b7;
     }
     public static final class integer {
         private integer() {}
@@ -125,7 +125,7 @@ public final class R {
     public static final class string {
         private string() {}
 
-        public static final int status_bar_notification_info_overflow = 0x7f0b0037;
+        public static final int status_bar_notification_info_overflow = 0x7f0b0036;
     }
     public static final class style {
         private style() {}
